ba46088b06413de414eb7fdf99baaf9b31590e5a
bodi44 <stogrinec.bodia@gmail.com>
2019-09-06 11:07:40 +0300

    add emailRegExp constant

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,14 +1,15 @@
 const phoneRegExp = /(([+][0-9]{1,3})|([0-9]{4}))\s*[\s]?[0-9]{1,3}([\s]?[0-9]{3})([\s]?[0-9]{3,4})/;
+const emailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
 export const validateEmail = (value, form) => {
   let error;
   if (!value && !phoneRegExp.test(form.values.phoneNumber)) {
     error = 'Required!';
   } else if (phoneRegExp.test(form.values.phoneNumber) && value) {
-    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    if (!emailRegExp.test(value)) {
       error = 'Invalid email!';
     }
-  } else if (!phoneRegExp.test(form.values.phoneNumber) && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+  } else if (!phoneRegExp.test(form.values.phoneNumber) && !emailRegExp.test(value)) {
     error = 'Invalid email';
   }
 
@@ -17,13 +18,13 @@ export const validateEmail = (value, form) => {
 
 export const validatePhoneNumber = (value, values) => {
   let error;
-  if (!value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+  if (!value && !emailRegExp.test(values.email)) {
     error = 'Required!';
-  } else if (value && /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+  } else if (value && emailRegExp.test(values.email)) {
     if (!phoneRegExp.test(value)) {
       error = 'Invalid phone!';
     }
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email) && !phoneRegExp.test(value)) {
+  } else if (!emailRegExp.test(values.email) && !phoneRegExp.test(value)) {
     error = 'Invalid phone!';
   }
 
